ba748ef5640c78570d3f5c13a1f1b9587ba088da
gabrielAraujoUni9 <gabriel.servicos.celular@gmail.com>
2024-11-14 19:51:43 -0300

    Update Work.js

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -7,28 +7,27 @@ const Work = () => {
   const workInfoData = [
     {
       image: PickMeals,
-      title: "Pick Meals",
-      text: "Lorem ipsum dolor sit amet consectetur. Maecenas orci et sagittis duis elementum interdum facilisi bibendum.",
+      title: "Pronto para Comer",
+      text: "Sua comida vem pronta para degustar e aproveitar o seu tempo de pausa.",
     },
     {
       image: ChooseMeals,
-      title: "Choose How Often",
-      text: "Lorem ipsum dolor sit amet consectetur. Maecenas orci et ",
+      title: "Faça sua Dieta",
+      text: "Escolha o que você precisa sem precisar trocar seu cardápio.",
     },
     {
       image: DeliveryMeals,
-      title: "Fast Deliveries",
-      text: "Lorem ipsum dolor sit amet consectetur. Maecenas orci et lorem ipsum",
+      title: "Entrega Rápida",
+      text: "Fazemos entregas rápidas para que você não perca o horário.",
     },
   ];
   return (
     <div className="work-section-wrapper">
       <div className="work-section-top">
-        <p className="primary-subheading">Work</p>
-        <h1 className="primary-heading">How It Works</h1>
+        <p className="primary-subheading">Nosso trabalho</p>
+        <h1 className="primary-heading">Como funciona?</h1>
         <p className="primary-text">
-          Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
-          elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
+          Aprenda como trazemos o melhor para as suas refeições e rotina.
         </p>
       </div>
       <div className="work-section-bottom">
